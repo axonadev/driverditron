eca95399482e4dfe01c1b4b9ba2b128bd078a712
Adekro <e.croce88@gmail.com>
2024-10-02 11:25:05 +0200

    vercel rout test

--- src/components/PrintOnLine/PrintOnLine.jsx
+++ src/components/PrintOnLine/PrintOnLine.jsx
@@ -1,44 +1,37 @@
-import React,{useRef} from "react";
+import React, { useRef } from "react";
 import { useParams } from "react-router-dom";
 
-const PrintOnLine=()=>{
+const PrintOnLine = () => {
+  const comandi = useRef();
+  const { dns } = useParams();
 
-    const comandi = useRef();
-    const { dns } = useParams();
+  const submithandler = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain");
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append("Access-Control-Allow-Headers", "*");
 
-    const submithandler = () =>{
+    const raw = "clear\nresprn\nINFO CODICE=1\nwecfine\n";
 
-        console.log(dns);
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+      mode: "no-cors",
+    };
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "text/plain");
-        myHeaders.append("Access-Control-Allow-Origin", "*");
-        myHeaders.append("Access-Control-Allow-Headers", "*");
+    fetch("https://" + dns + "/cmd/wec", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  };
 
-        const raw = "clear\nresprn\nINFO CODICE=1\nwecfine\n";
-
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow",
-            mode:"no-cors"
-        };
-
-        fetch("https://" + dns + "/cmd/wec", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
-    }
-
-    return(
-       
-        <div>
-            <textarea ref={comandi} />
-            <button onClick={submithandler}>submit</button>
-        </div>
-       
-    )
-}
+  return (
+    <div>
+      <textarea ref={comandi} />
+      <button onClick={submithandler}>submit</button>
+    </div>
+  );
+};
 export default PrintOnLine;
-
